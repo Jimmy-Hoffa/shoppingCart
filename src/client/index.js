465015ed3bf1121425b7cf7327f5f4ce36a8588f
Jimmy-Hoffa <94380480+Jimmy-Hoffa@users.noreply.github.com>
2021-11-23 10:44:43 -0500

    do it

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,5 @@ import { createElement } from 'lwc';
 import MyApp from 'my/app';
 
 const app = createElement('my-app', { is: MyApp });
-// eslint-disable-next-line @lwc/lwc/no-document-query
+// eslint-disable-next-line @lwc/lwc/no-document-queryasdfa
 document.querySelector('#main').appendChild(app);
